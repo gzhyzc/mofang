ea9a7b1695904b287a703884ce85bc937c5e3bb7
gzhyzc <91192280+gzhyzc@users.noreply.github.com>
2021-09-22 21:26:35 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git333");
         System.out.println("hello git444");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
