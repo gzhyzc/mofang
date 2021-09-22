653ca8a0e9108804c1ad18a5afeb27d9764e801b
yangyan <1051117332@qq.com>
2021-09-22 20:45:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Yangyan
+ * @create 2021-09-22 20:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
